17c829518788fa15c2dd1f43d8d909466acfbd2c
taylordunn <t.dunn19@gmail.com>
2022-01-06 17:46:04 -0400

    Updated get_provinces docs and tests

--- R/provinces.R
+++ R/provinces.R
@@ -1,23 +1,32 @@
 #' Get provinces and territories
 #'
+#' @param geo_only Logical, indicating if only provinces/territories should be
+#'   returned. If FALSE, also returned non-geographic entities like
+#'   Repatriated Canadians and the Federal Allocation for vaccinations.
+#'
 #' @return A data frame with a row per province/territory.
 #' @export
 #'
 #' @examples
 #'
 #' get_provinces()
-#'
+#' get_provinces(geo_only = FALSE)
 #' @importFrom dplyr bind_rows mutate
 #' @importFrom rlang .data
-get_provinces <- function() {
+get_provinces <- function(geo_only = TRUE) {
   base_url <- "https://api.covid19tracker.ca/provinces"
-  api_params <- ""
+  if (geo_only) {
+    api_params <- "?geo_only=true"
+  } else {
+    api_params <- ""
+  }
   url <- paste0(base_url, api_params)
 
   content_parsed <- get_content_parsed(url)
 
   dplyr::bind_rows(content_parsed) %>%
     dplyr::mutate(
+      # Use logical type instead of 0/1
       geographic = .data$geographic == 1,
       updated_at = as.POSIXct(.data$updated_at)
     )
--- tests/testthat/test-provinces.R
+++ tests/testthat/test-provinces.R
@@ -1,10 +1,18 @@
 test_that("get_provinces", {
   request_sleep()
   provinces <- get_provinces()
-
-  expect_equal(nrow(provinces), 16)
+  expect_equal(nrow(provinces), 13)
   expect_equal(ncol(provinces), 10)
   expect_setequal(provinces$code,
+                  c("ON", "QC", "NS", "NB", "MB", "BC", "PE", "SK", "AB",
+                    "NL", "NT", "YT", "NU"))
+
+  request_sleep()
+  provinces_geo_false <- get_provinces(geo_only = FALSE)
+
+  expect_equal(nrow(provinces_geo_false), 16)
+  expect_equal(ncol(provinces_geo_false), 10)
+  expect_setequal(provinces_geo_false$code,
                   c("ON", "QC", "NS", "NB", "MB", "BC", "PE", "SK", "AB",
                     "NL", "NT", "YT", "NU",
                     "_RC", "FA", "NFR"))
